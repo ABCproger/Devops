f98318d577f6a530d6afb9a909bde60e720ef8ed
Vladyslav <vladmelhnik@gmail.com>
2024-11-16 11:42:22 +0200

    init commit

---
+++ FuncA.h
@@ -0,0 +1,18 @@
+#ifndef TRIGFUNCTION_H
+#define TRIGFUNCTION_H
+
+class TrigFunction {
+public:
+    double FuncA(); // Function returning 0
+};
+
+#endif
+#ifndef TRIGFUNCTION_H
+#define TRIGFUNCTION_H
+
+class TrigFunction {
+public:
+    double FuncA(); // Function returning 0
+};
+
+#endif
